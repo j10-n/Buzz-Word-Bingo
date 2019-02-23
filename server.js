8d0ace948b510bbe0a85091e3ff1f1492d23e238
justen <justen@justens-MacBook-Pro.local>
2019-02-22 14:46:50 -1000

    finished mvp

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
 
-let buzzwords = [];
+let buzzwords = {};
+let totalScore = 0;
 
 app.use(express.static("public"));
 // parse application/x-www-form-urlencoded
@@ -20,15 +21,59 @@ app
   })
   .post(function(req, res) {
     let word = req.body.buzzWord;
+    let points = req.body.points;
+    let keys = Object.keys(buzzwords);
     console.log(buzzwords);
-    if (buzzwords.includes(word)) {
-      res.send({ sucess: false });
+    // console.log(points);
+    if (keys.includes(word)) {
+      res.send({ success: false });
     } else {
-      buzzwords.push(word);
-      res.send({ sucess: true });
+      buzzwords[word] = points;
+      res.send({ success: true });
+    }
+  })
+  .put(function(req, res) {
+    let word = req.body.buzzWord;
+    let points = req.body.points;
+    let keys = Object.keys(buzzwords);
+
+    if (keys.includes(word)) {
+      buzzwords[word] = points;
+      res.send({ success: true });
+    } else {
+      res.send({ success: false });
+    }
+  })
+  .delete(function(req, res) {
+    let word = req.body.buzzWord;
+    let keys = Object.keys(buzzwords);
+
+    if (keys.includes(word)) {
+      delete buzzwords[word];
+      res.send({ success: true });
+    } else {
+      res.send({ success: false });
     }
   });
-//   .put(function(req, res) {})
-//   .delete(function(req, res) {});
 
-app.listen(PORT, () => console.log(`working on port ${PORT}`));
+app.post("/reset", function(req, res) {
+  buzzwords = {};
+  totalScore = 0;
+  res.send({ success: true });
+});
+
+app.post("/heard", function(req, res) {
+  let word = req.body.buzzWord;
+  let points = buzzwords[word];
+  let keys = Object.keys(buzzwords);
+
+  if (keys.includes(word)) {
+    totalScore += points;
+    res.send({ success: true });
+    console.log("totalScore: ", totalScore);
+  } else {
+    res.send({ success: false });
+  }
+});
+
+app.listen(PORT, () => console.log(`running on port ${PORT}`));
